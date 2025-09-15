87e9c17d8c1d37e467862ac22a9298a870fd48d9
Oscar Checa <ochecaa21@gmail.com>
2025-09-15 11:47:36 -0500

    feat(layout): add main layout.

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,32 @@
+<script setup lang="js">
+
+import {ref} from "vue";
+import LanguageSwitch from "./language-switch.vue";
+
+const drawerVisible = ref(false);
+const toggleDrawer = () => {
+    drawerVisible.value = !drawerVisible.value;
+};
+</script>
+
+<template>
+<div>
+  <div>
+    <pv-menubar>
+      <template #start>
+        <pv-button icon="pi pi-bars" label="Catchup" text @click="toggleDrawer" />
+      </template>
+      <template #end>
+        <language-switch></language-switch>
+        </template>
+    </pv-menubar>
+  </div>
+</div>
+  <div>
+    <!-- TODO: Article List -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
