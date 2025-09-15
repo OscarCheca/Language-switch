5372753266210208226a5ff61d9a0a5d8053b45c
Oscar Checa <ochecaa21@gmail.com>
2025-09-15 11:35:51 -0500

    feat(layout): add footer content.

---
+++ src/shared/presentation/components/footer-content.vue
@@ -0,0 +1,26 @@
+<script setup lang="js">
+ import {useI18n} from "vue-i18n";
+
+ const {t} = useI18n();
+</script>
+
+<template>
+  <div class="grid bg-primary mt-4 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2025. ACME Studios</p>
+      <div class="col-12 ml-3 align-items-center justify-content-center">
+        <p>
+          {{ t('authoring-phrase.intro')}}<i class="pi pi-heart"/>
+          {{ t('authoring-phrase.use')}}<a href="https://primevue.org" target="_blank"/>
+          {{ t('authoring-phrase.author', { brand: 'ACME'})}}
+        </p>
+        <p>{{ t('footer.powered-by')}} <a href="https://www.newsapi.org">NewsAPI.org</a></p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
